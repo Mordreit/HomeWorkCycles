0ef581c51d264816d140377a78df1af38534be53
Mordreit <spirit_x93@mail.ru>
2022-05-09 19:28:33 +0400

    Готовое дз!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Задание 1
+        int i = 1;
+        while (i <11) {
+            System.out.print(i + " ");
+            i++;
+        }
+        System.out.println();
+        for (int e = 10; e >= 1; e--) {
+            System.out.print(e + " ");
+        }
+        System.out.println();
+
+        // Задание 2
+
+        for (int friday = 5; friday <= 31; friday = friday + 7) {
+            System.out.println("Сегодня пятница, " + friday + ". Необходимо подготовить отчет.");
+        }
+
+        // Задание 3
+
+        int year = 2021;
+        int a = year - 200;
+        int b = year + 100;
+        for (; a < b; a++) {
+            if (a % 79 == 0) {
+                System.out.println(a);
+            }
+        }
+
+
+
+
     }
 }
